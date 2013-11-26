d64268da70a805f189818eab72af3a4cf00b8725
smmalloy <malloyj@btinternet.com>
2013-11-26 12:15:10 +0000

    Tutorial sheet 2+3

---
+++ TutorialSheet2/src/Author.java
@@ -0,0 +1,57 @@
+public class Author{
+		
+		private String firstname;
+		private String surname;
+		private String nationality;
+		private String genre;
+	
+		
+	public String getFirstName()
+	{
+		return this.firstname;
+	}
+	public String getSurname()
+	{
+		return this.surname;
+	}
+	public String getNationality()
+	{
+		return this.nationality;
+	}
+	public String getGenre()
+	{
+		return this.genre;
+	}
+	public void setFirstName()
+	{
+		 firstname = "James";
+	}
+	public void setSurname()
+	{
+		 surname = "Allen";
+	}
+	public void setNationality()
+	{
+	 nationality = "British";
+	}
+	public void setGenre()
+	{
+	 genre = "Novel";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
---
+++ TutorialSheet2/src/Book.java
@@ -0,0 +1,34 @@
+	public class Book extends Author{
+		
+
+		private String title;
+		private String format;
+		private int ISBN;
+	 
+	public void book(String author, String title, String format, int ISBN){
+		
+		
+	this.title = title;
+	this.format = format;
+	this.ISBN = ISBN;
+	
+	}
+	
+	public String getauthor()
+	{
+		return getauthor();
+	}
+	public String gettitle()
+	{
+		return title;
+	}
+	public String getformat()
+	{
+		return format;
+	}
+	public int getISBN()
+	{
+		return ISBN;
+	}
+	}
+	
