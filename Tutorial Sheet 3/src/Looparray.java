97873617435673593a1424fbdeff43e0c21aed02
smmalloy <malloyj@btinternet.com>
2013-11-26 12:27:37 +0000

    Initial commit Tutorial 3

---
+++ Tutorial Sheet 3/src/Looparray.java
@@ -0,0 +1,10 @@
+public class Looparray {
+	   public static void main(String[] args){
+	         for(int i=1; i<6; i++){
+	        	 System.out.println( i + 10);
+	              System.out.println("Count is: " + i);
+	           
+	         }
+	    }
+	}
+
